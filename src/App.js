d47dd3ae5218f7e1f0335a687b289c85218a8ad4
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-06-30 22:14:14 +0900

    Todo作成の機能を追加.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,41 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
-  render() {
-    const tasks = [
-      { title: 'Todo１つ目', id: 0 },
-      { title: 'Todo２つ目', id: 1 },
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          title: 'デフォルトTODO',
+          id: 0
+        },
+      ],
+      uniqueId: 1,
+    };
+    this.addTodo = this.addTodo.bind(this);
+  }
+  addTodo(title) {
+    const {
+      tasks,
+      uniqueId,
+    } = this.state;
 
+    tasks.push({
+      title,
+      id: uniqueId,
+    });
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1,
+    })
+  }
+  render() {
     return (
       <div>
         <h1>Todo App</h1>
-        <TodoInput />
-        <TodoList tasks={tasks} />
+        <TodoInput addTodo={this.addTodo} />
+      <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class TodoInput extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+  handleClick() {
+    const inputValue = this.state.inputValue;
+    this.props.addTodo(inputValue);
+  }
   render() {
     return (
       <div>
-        <input placeholder='新規TODOを入力してください' />
-        <button>登録</button>
+        <input placeholder='新規TODOを入力してください' value={this.state.inputValue} onChange={this.handleChange} />
+        <a href="/regster" onClick={this.handleClick}>登録</a>
       </div>
     )
   };
