770b6af0436142acc743fde221de16fa33063f05
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2020-07-03 23:41:28 +0900

    Reducerを実装。Animate.cssを導入.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  task: '',
+  tasks: [],
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Reducerを作成
+// アクションタイプによって処理を切り分ける
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        task: action.payload.task,
+      };
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.type]),
+      };
+    default:
+      return state;
+  }
+}
+
+// Storeを作成する
+// ReduxのStoreを生成する場合、Reducerが必要となる
+const store = createStore(tasksReducer);
+
+// アクションタイプを変数化する
+const INPUT_TASK = 'INPUT_TASK';
+const ADD_TASK = 'ADD_TASK';
+
+// タスク入力用のActionCreator
+const inputTask = (task) => ({
+  type: INPUT_TASK,
+  payload: {
+    task,
+  },
+});
+
+// タスク追加用のActionCreator
+const addTask = (task) => ({
+  type: ADD_TASK,
+  payload: {
+    task,
+  },
+});
+
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => store.dispatch(inputTask(e.target.value))}
+      />
+      <input
+        type="button"
+        value="add"
+        onClick={() => store.dispatch(addTask(task))}
+      />
+      <ul>
+        {tasks.map(function (item, i) {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store) {
+  render(
+    <TodoApp store={store} />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
